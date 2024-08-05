e8841e3275cbc74b9c10d9d428ffbf7d3a2b7e63
Emanoel <Emanoel@DESKTOP-F1C0VMI>
2024-08-05 20:53:21 -0300

    Add project files.

---
+++ BibliotecaExercicio/Library.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaExercicio
+{
+    internal class Library
+    {
+        private string name;
+        public string Name { get; set; }
+
+        private List<Book> books;
+        
+        private Dictionary<string, Book> category;
+
+        public Library(string name)
+        {
+
+        }
+        public Library() { }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void AddBook(Book book, string category)
+        {
+
+        }
+
+        public void RemoveBook(Book book) 
+        {
+            
+        }
+
+        public List<Book> GetBooks() 
+        {
+            return books;
+        }
+
+        public List<Book> GetBooks(string category) 
+        {
+            return books;
+        }
+    }
+}
---
+++ BibliotecaExercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿
+List<string> livros = new List<string>();
+
+while(true)
+{
+    Console.WriteLine("[1] Adicionar livro a biblioteca");
+    Console.WriteLine("[2] Remover livro da biblioteca");
+    Console.WriteLine("[3] Ver todos os livros adicionados");
+
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("\nDigite o nome do livro");
+            string livro = Console.ReadLine();
+            livros.Add(livro);
+            break;
+        case 2:
+            Console.WriteLine("\nDigite o id no livro a ser removido");
+            int livroId = Convert.ToInt32(Console.ReadLine());
+
+            break;
+        case 3:
+
+            break;
+        default:
+            Console.WriteLine("\n Opção invalida");
+            break;
+    }
+}
